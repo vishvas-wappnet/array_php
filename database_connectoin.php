63a1d20ad8bb5be79b20dfa33a26cb59fa166d05
vishvas-wappnet <vishvas.wappnet@gmail.com>
2023-02-13 20:21:10 +0530

    database connectoin established

--- database_connectoin.php
+++ database_connectoin.php
@@ -1,7 +1,4 @@
-<html>
 
-<body> 
-    <center>
 
                     <?php
 
@@ -9,7 +6,10 @@
 
 
 
-
+                                               
+                                         $reg_username = 'anon_1277';
+                                         $reg_user_email='anon@hacker.com';
+                                         $reg_user_password='youarehacked';                                     
                                                                                                             
                                                $servername = "localhost";
                                                $username = "root";
@@ -17,22 +17,35 @@
                                                $database = "test";
 
                                                 // Create connection
-                                               $conn = mysqli_connect($servername, $username, $password ,$database);
+                                               $conn =  mysqli_connect($servername, $username, $password ,$database);
 
                                                // Check connection
                                                  if (!$conn) 
                                                       {
                                                          die("Connection failed: " . mysqli_connect_error());
                                                       }
-                                                echo " Database Connected successfully";
+                                                else
+                                                     {
+                                                        echo " Database Connected successfully" . "<br>";
+                                                     }                 
+
+                                                 
+                                                 $sql1 = 'INSERT INTO user VALUES (1, "anon1277", "anon1277hacker.com", "hackme")' ;
+                                                 mysqli_query($conn, $sql1); 
                                                                 
-
-
+                                                if($conn->query($sql1 == TRUE))
+                                                   {
+                                                      echo "user registred successfully " ;
+                                                   }
+                                    
+                                                else 
+                                                {
+                                                   echo "Error: " . $sql1 . "<br>" . $conn->error;
+                                                }
                     ?>
 
  
-<center>
 
-</body>
 
-</html>
+
+
