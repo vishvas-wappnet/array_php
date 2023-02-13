fd0e73a5ef44e661a6c94b713654096ad5f84866
vishvas-wappnet <vishvas.wappnet@gmail.com>
2023-02-13 13:10:55 +0530

    Done

---
+++ php_array.php
@@ -0,0 +1,12 @@
+<?php
+echo "------------------------PHP ARRAY FUNCTION--------------------" . "<br>";
+
+
+$array_names = array("Vishvas" , "Ronak" , "Isha" , "Arpit"); //creating  array of names  
+
+echo "getting first element of array is: :" . $array_names[0]  . "<br>"; //retrivniig first element of  array 
+
+
+echo "getting second element of array is: :" . $array_names[1]  . "<br>"; //retrivniig Second element of  array 
+
+?>
