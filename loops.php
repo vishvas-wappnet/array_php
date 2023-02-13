329073c2d6360ece7c6d1a1bf504a265deb24132
vishvas-wappnet <vishvas.wappnet@gmail.com>
2023-02-13 19:15:29 +0530

    final commit on loops program

--- loops.php
+++ loops.php
@@ -7,13 +7,56 @@
 
                 echo "basic for loop example" . "<br>"  ;
 
-                 for ($i=0 ; $i<=10 ;$i++)
+                 for ($i=0 ; $i<=10 ;$i++) //for loop
                         {
                             echo"printing 10 users from database  : user" . $i . "</br>";
                         }
+
+                
+                        echo "</br>";
+                        echo "</br>";
+                        echo "</br>";
+                        echo "</br>";
+                        echo "</br>";        
+
+                echo "while loop example". "<br>";
+                $number = 1;
+                while ($number <= 10 )//while loop fro printing 10 user 
+                {
+                        echo"printing   user" . $number. "</br>";
+                        $number++;
+                }
+                 
+
+
+                echo "</br>";
+                echo "</br>";
+                echo "</br>";
+                echo "</br>";
+                echo "</br>";
+
+                echo "while loop example". "<br>";
+                $number = 1;
+               //while loop fro printing 10 user 
+               do 
+                  {
+                        echo"printing   user using do while loop" . $number. "</br>";
+                        $number++;
+                  }
+                while ($number <= 10 ); 
+
+                echo "</br>";
+                echo "</br>";
+                echo "</br>";
+                echo "</br>";
+                echo "</br>";
+
+
+
+
                 echo "printing names using fore each loop:"  . "<br>" ;  
                 $names_array = array(" dixita" , "jayprakash" , "Vishvas" , "Ronak" , "Isha" , "Arpit");
-                foreach($names_array as $names) //printing sorted array using fore each loop
+                foreach($names_array as $names) //printing users  using fore each loop
 
                 {
                 echo("$names<br/>") ;
