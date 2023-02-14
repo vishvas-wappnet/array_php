2807a2993b3a77f0f94ec773aeaa909b8f9c81e4
vishvas-wappnet <vishvas.wappnet@gmail.com>
2023-02-14 11:42:52 +0530

    insert operatoin done

--- database_connectoin.php
+++ database_connectoin.php
@@ -1,47 +1,53 @@
 
+ <?php
 
-                    <?php
-
-                                         echo "-----------------------------------------------database connection program-----------------------------------------------" ."<br>" ; 
+            echo "-----------------------------------------------database connection program-----------------------------------------------" ."<br>" ; 
 
 
 
                                                
-                                         $reg_username = 'anon_1277';
-                                         $reg_user_email='anon@hacker.com';
-                                         $reg_user_password='youarehacked';                                     
+                $reg_username = 'anon_1277';
+               $reg_user_email='anon@hacker.com';
+               $reg_user_password='youarehacked';                                     
                                                                                                             
-                                               $servername = "localhost";
-                                               $username = "root";
-                                               $password = "";
-                                               $database = "test";
+               $servername = "localhost";
+               $username = "root";
+               $password = "";
+               $database = "test";
 
-                                                // Create connection
-                                               $conn =  mysqli_connect($servername, $username, $password ,$database);
+               // Create connection
+               $conn =  mysqli_connect($servername, $username, $password ,$database);
 
-                                               // Check connection
-                                                 if (!$conn) 
-                                                      {
-                                                         die("Connection failed: " . mysqli_connect_error());
-                                                      }
-                                                else
-                                                     {
-                                                        echo " Database Connected successfully" . "<br>";
-                                                     }                 
+                // Check connection
+               if (!$conn) 
+                   {
+                        die("Connection failed: " . mysqli_connect_error());
+                   }
+                else
+                    {
+                        echo " Database Connected successfully" . "<br>";
+                    }                 
 
                                                  
-                                                 $sql1 = 'INSERT INTO user VALUES (1, "anon1277", "anon1277hacker.com", "hackme")' ;
-                                                 mysqli_query($conn, $sql1); 
-                                                                
-                                                if($conn->query($sql1 == TRUE))
-                                                   {
-                                                      echo "user registred successfully " ;
-                                                   }
-                                    
-                                                else 
-                                                {
-                                                   echo "Error: " . $sql1 . "<br>" . $conn->error;
-                                                }
+               $sql1 = 'INSERT INTO users VALUES (null ,"an11277", "anon1277@hacker.com", "hackme1")' ;
+              
+                 
+               
+               if( mysqli_query($conn, $sql1) )
+
+              // if(mysqli_error($conn))
+               {
+                 //  echo mysqli_error($conn);
+                 print "sucess";
+               }
+         
+                             
+               else 
+                   {
+                     echo " erroruser registered succefully " ;
+                   }
+
+                         $conn->close();
                     ?>
 
  
